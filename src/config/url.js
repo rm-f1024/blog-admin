f10e0a620fa364cdc2b37e960bb7718f16e82a7a
Alex <614647621@qq.com>
2022-08-12 18:49:13 +0800

    123

--- src/config/url.js
+++ src/config/url.js
@@ -1,5 +1,5 @@
 const {NODE_ENV} = process.env
 
-const local= 'localhost:7001'
+const local= 'localhost:7777'
 const ipUrl = NODE_ENV==='production'?  'http://106.52.41.19/api/admin':`http://${local}/api/admin`
 export default ipUrl
